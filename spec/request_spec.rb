6c867cb23ae62cfd52b1182f516898aa5c5b771d
Alfred Nagy <alfrednag@gmail.com>
2016-10-25 12:55:41 +0100

    Fix bug in the swagger file checker that raised error when response definition has no schema

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -1,58 +1,65 @@
 require 'spec_helper'
 
 describe "Example API", type: :apivore, order: :defined do
-  subject { Apivore::SwaggerChecker.instance_for("/swagger-doc.json") }
+  swagger_checkers = [
+    Apivore::SwaggerFileChecker.instance_for(File.expand_path("../data/01_sample2.0.json", __FILE__)),
+    Apivore::SwaggerChecker.instance_for("/swagger-doc.json")
+    ]
+  swagger_checkers.each do |swagger_checker|
+    subject { swagger_checker }
+    describe "using #{swagger_checker.class}" do
+      context "has valid paths" do
+        it 'allows the same path (with response documented) to be tested twice' do
+          expect(subject).to validate(:get, "/services.json", 200)
+          expect(subject).to validate(:get, "/services.json", 200)
+        end
 
-  context "has valid paths" do
-    it 'allows the same path (with response documented) to be tested twice' do
-      expect(subject).to validate(:get, "/services.json", 200)
-      expect(subject).to validate(:get, "/services.json", 200)
-    end
+        it do
+          expect(subject).to validate(
+            :post, "/services.json", 204, {'name' => 'hello world'}
+          )
+        end
 
-    it do
-      expect(subject).to validate(
-        :post, "/services.json", 204, {'name' => 'hello world'}
-      )
-    end
+        it do
+          expect(subject).to validate(
+            :get, "/services/{id}.json", 200, {'id' => 1}
+          )
+        end
 
-    it do
-      expect(subject).to validate(
-        :get, "/services/{id}.json", 200, {'id' => 1}
-      )
-    end
+        it do
+          expect(subject).to validate(
+            :put, "/services/{id}.json", 204, {'id' => 1}
+          )
+        end
 
-    it do
-      expect(subject).to validate(
-        :put, "/services/{id}.json", 204, {'id' => 1}
-      )
-    end
+        it do
+          expect(subject).to validate(
+            :delete, "/services/{id}.json", 204, {'id' => 1}
+          )
+        end
 
-    it do
-      expect(subject).to validate(
-        :delete, "/services/{id}.json", 204, {'id' => 1}
-      )
-    end
+        it do
+          expect(subject).to validate(
+            :patch, "/services/{id}.json", 204, {'id' => 1}
+          )
+        end
+      end
 
-    it do
-      expect(subject).to validate(
-        :patch, "/services/{id}.json", 204, {'id' => 1}
-      )
-    end
-  end
+      context 'and' do
+        it 'has had all documented routes tested' do
+          expect(subject).to validate_all_paths
+        end
 
-  context 'and' do
-    it 'has had all documented routes tested' do
-      expect(subject).to validate_all_paths
+        it 'additionally conforms to a custom schema' do
+          expect(subject).to conform_to(Apivore::CustomSchemaValidator::WF_SCHEMA)
+        end
+        # it 'has definitions consistent with the master docs' do
+        #   expect(subject).to be_consistent_with_swagger_definitions(
+        #     "api.westfield.io", 'deal'
+        #   )
+        # end
+      end
     end
-
-    it 'additionally conforms to a custom schema' do
-      expect(subject).to conform_to(Apivore::CustomSchemaValidator::WF_SCHEMA)
-    end
-    # it 'has definitions consistent with the master docs' do
-    #   expect(subject).to be_consistent_with_swagger_definitions(
-    #     "api.westfield.io", 'deal'
-    #   )
-    # end
   end
 
 end
